37d72e47a43d3ff8c7053e03bd213420feaae44d
zhuxj <xazhuxj@qq.com>
2020-09-28 11:54:32 +0800

    修改好三个测量函数，加入了测试代码

--- SurMath/SurMath.cs
+++ SurMath/SurMath.cs
@@ -4,28 +4,111 @@ namespace ZXY
 {
     public static class SurMath
     {
+        public const double PI = Math.PI;
+        public const double TWOPI = 2 * Math.PI;
+        public const double TODEG = 180.0 / PI;
+        public const double TORAD = PI / 180.0;
+        public const double TOSECOND = 180.0 * 3600.0 / PI;
+
         /// <summary>
         /// 度分秒角度化弧度
+        /// 此处需讲解 ref、out、元组
+        /// 元组功能在 C# 7.0 及更高版本中可用，它提供了简洁的语法，用于将多个数据元素分组成一个轻型数据结构。
         /// </summary>
-        /// <param name="">度分秒角度:1.2030</param>
-        /// <returns>弧度</returns>
-        public static double DMStoRad(double dmsAngle)
+        /// <param name="dmsAngle">度分秒角度:1.2030</param>
+        /// <returns>(度, 分, 秒)</returns>
+        public static (int d, int m, double s) DMStoDMS(double dmsAngle)
         {
-            int d = (int)dmsAngle;
-            dmsAngle = (dmsAngle - d) * 100;
-            int m = (int)dmsAngle;
-            double s = (dmsAngle - m) * 100;
-            return (d+m/60.0+s/3600.0)/180.0*Math.PI;
+            //错误的算法
+            //int d = (int)dmsAngle;
+            //dmsAngle = (dmsAngle - d) * 100;
+            //int m = (int)dmsAngle;
+            //double s = (dmsAngle - m) * 100;
+            //return (d + m / 60.0 + s / 3600.0) * TORAD;
+
+            //正确的算法
+            dmsAngle *= 10000;
+            int angle = (int)dmsAngle;
+            int d = angle / 10000;
+            angle -= d * 10000;
+            int m = angle / 100;
+            double s = dmsAngle - d * 10000 - m * 100;
+            return (d, m, s);
         }
 
         /// <summary>
-        /// 弧度化度分秒角度
+        /// 度分秒角度值1.02305 化 度、分、秒字符串 1°02′30.5″
         /// </summary>
-        /// <param name="">弧度</param>
+        /// <param name="dmsAngle">度分秒角度:1.02305</param>
+        /// <returns>度、分、秒字符串 1°02′30.5″</returns>
+        public static string DMStoString(double dmsAngle)
+        {
+            int f = Math.Sign(dmsAngle);
+            var dms = DMStoDMS(dmsAngle);
+            if (Math.Abs(dms.s) <= 1e-10)
+                return $"{dms.d}°{f * dms.m:00}′00″";
+            else
+                return $"{dms.d}°{f * dms.m:00}′{f * dms.s:00.######}″";
+        }
+
+        /// <summary>
+        /// 度分秒角度化弧度
+        /// </summary>
+        /// <param name="dmsAngle">度分秒角度:1.2030</param>
+        /// <returns>弧度</returns>
+        public static double DMStoRad(double dmsAngle)
+        {
+            var dms = DMStoDMS(dmsAngle);
+            return (dms.d + dms.m / 60.0 + dms.s / 3600.0) * TORAD;
+        }
+
+        /// <summary>
+        /// 弧度（radian）转化为 度、分、秒
+        /// </summary>
+        /// <param name="radAngle">弧度角度值</param>
+        /// <returns>度分秒角度:1.2030</returns>
+        public static (int d, int m, double s) Radian2DMS(double radAngle)
+        {
+            //radAngle *= TODEG;
+            //int d = (int)radAngle;
+            //radAngle = (radAngle - d) * 60;
+            //int m = (int)radAngle;
+            //double s = (radAngle - m) * 60;
+            //return (d + m / 100.0 + s / 10000.0);
+
+            radAngle *= TOSECOND;
+            int angle = (int)radAngle;
+            int d = angle / 3600;
+            angle -= d * 3600;
+            int m = angle / 60;
+            double s = radAngle - d * 3600 - m * 60;
+            return (d, m, s);
+        }
+
+        /// <summary>
+        /// 弧度（radian）化度分秒角度
+        /// </summary>
+        /// <param name="radAngle">弧度角度值</param>
         /// <returns>度分秒角度:1.2030</returns>
         public static double RadtoDMS(double radAngle)
         {
-            return 0;
+            var dms = Radian2DMS(radAngle);
+            return (dms.d + dms.m / 100.0 + dms.s / 10000.0);
+        }
+
+        /// <summary>
+        /// 弧度（radian）化 度、分、秒字符串 1°02′30.5″
+        /// </summary>
+        /// <param name="radAngle">弧度角度值</param>
+        /// <returns>度、分、秒字符串 1°02′30.5″</returns>
+        public static string RadtoString(double radAngle)
+        {
+            int f = Math.Sign(radAngle);
+            var dms = Radian2DMS(radAngle);
+            if (Math.Abs(dms.s) <= 1e-10)
+                return $"{dms.d}°{f * dms.m:00}′00″";
+            else
+                return $"{dms.d}°{f * dms.m:00}′{f * dms.s:00.######}″";
         }
 
         /// <summary>
@@ -35,10 +118,12 @@ namespace ZXY
         /// <param name="yA">A的Y坐标</param>
         /// <param name="xB">B的X坐标</param>
         /// <param name="yB">B的Y坐标</param>
-        /// <returns>A->两点的坐标方位角，单位：弧度</returns>
+        /// <returns>A->B的坐标方位角，单位：弧度</returns>
         public static double Azimuth(double xA, double yA, double xB, double yB)
         {
-            return 0;
+            double dx = xB - xA;
+            double dy = yB - yA;
+            return Math.Atan2(dy, dx) + (dy < 0 ? 1 : 0) * TWOPI;
         }
     }
 }
--- UnitTestSurMath/UnitTestSurMath.cs
+++ UnitTestSurMath/UnitTestSurMath.cs
@@ -8,11 +8,143 @@ namespace UnitTestSurMath
         [TestMethod]
         public void TestDMStoRad()
         {
+            {
+                double rad = ZXY.SurMath.DMStoRad(1.2030);
+                Assert.AreEqual(0.0234165007975906, rad, 1e-15);
+
+                rad = ZXY.SurMath.DMStoRad(-1.2030);
+                Assert.AreEqual(-0.0234165007975906, rad, 1e-15);
+            }
+
+            {
+                double rad = ZXY.SurMath.DMStoRad(1.4); //error 0.029282746339015973
+                Assert.AreEqual(0.02908882086657220, rad, 1e-15);
+
+                rad = ZXY.SurMath.DMStoRad(-1.4); //error 0.029282746339015973
+                Assert.AreEqual(-0.02908882086657220, rad, 1e-15);
+            }
+
+            {
+                double rad = ZXY.SurMath.DMStoRad(1.40001);
+                Assert.AreEqual(0.02908930568025330, rad, 1e-15);
+            
+                rad = ZXY.SurMath.DMStoRad(-1.40001);
+                Assert.AreEqual(-0.02908930568025330, rad, 1e-15);
+            }
+
+            {
+                double rad = ZXY.SurMath.DMStoRad(39.520071672); //39 52 0.71672
+                Assert.AreEqual(0.69580806988502100, rad, 1e-15);
+
+                rad = ZXY.SurMath.DMStoRad(-39.520071672); //39 52 0.71672
+                Assert.AreEqual(-0.69580806988502100, rad, 1e-15);
+            }
+        }
+
+        [TestMethod]
+        public void TestDMStoString()
+        {
+            {               
+                string dms = ZXY.SurMath.DMStoString(1.2030);
+                Assert.AreEqual<string>("1¡ã20¡ä30¡å", dms);                
+                
+                dms = ZXY.SurMath.DMStoString(-1.2030);
+                Assert.AreEqual<string>("-1¡ã20¡ä30¡å", dms);
+            }
+
+            {
+                string dms = ZXY.SurMath.DMStoString(1.4);
+                Assert.AreEqual<string>("1¡ã40¡ä00¡å", dms);
+
+                dms = ZXY.SurMath.DMStoString(-1.4);
+                Assert.AreEqual<string>("-1¡ã40¡ä00¡å", dms);
+            }
+
+            {
+                string dms = ZXY.SurMath.DMStoString(1.40001);
+                Assert.AreEqual<string>("1¡ã40¡ä00.1¡å", dms);
+
+                dms = ZXY.SurMath.DMStoString(-1.40001);
+                Assert.AreEqual<string>("-1¡ã40¡ä00.1¡å", dms);
+            }
+
+            {
+                string dms = ZXY.SurMath.DMStoString(39.520071672);
+                Assert.AreEqual<string>("39¡ã52¡ä00.71672¡å", dms);
+
+                dms = ZXY.SurMath.DMStoString(-39.520071672);
+                Assert.AreEqual<string>("-39¡ã52¡ä00.71672¡å", dms);
+            }
         }
 
         [TestMethod]
         public void TestRadtoDMS()
         {
+            {
+                double rad = ZXY.SurMath.RadtoDMS(0.0234165007975906);
+                Assert.AreEqual(1.2030, rad, 1e-10);               
+
+                rad = ZXY.SurMath.RadtoDMS(-0.0234165007975906);
+                Assert.AreEqual(-1.2030, rad, 1e-10);
+            }
+            {
+                double rad = ZXY.SurMath.RadtoDMS(0.02908882086657220); 
+                Assert.AreEqual(1.4, rad, 1e-10);
+               
+                rad = ZXY.SurMath.RadtoDMS(-0.02908882086657220 );
+                Assert.AreEqual(-1.4, rad, 1e-10);
+            }
+
+            {
+                double rad = ZXY.SurMath.RadtoDMS(-0.02908930568025330);
+                Assert.AreEqual(-1.40001 , rad, 1e-10);               
+
+                rad = ZXY.SurMath.RadtoDMS(0.02908930568025330);
+                Assert.AreEqual(1.40001, rad, 1e-10);
+            }
+
+            {
+                double rad = ZXY.SurMath.RadtoDMS(0.69580806988502100); //39 52 0.71672
+                Assert.AreEqual(39.520071672, rad, 1e-10);
+
+                rad = ZXY.SurMath.RadtoDMS(-0.69580806988502100); //39 52 0.71672
+                Assert.AreEqual(-39.520071672 , rad, 1e-10);
+            }
+        }
+
+        [TestMethod]
+        public void TestRadtoString()
+        {
+            {                
+                string dms = ZXY.SurMath.RadtoString(0.0234165007975906);
+                Assert.AreEqual<string>("1¡ã20¡ä30¡å", dms);               
+
+                dms = ZXY.SurMath.RadtoString(-0.0234165007975906);
+                Assert.AreEqual<string>("-1¡ã20¡ä30¡å", dms);
+            }
+            {                
+                string dms = ZXY.SurMath.RadtoString(0.02908882086657220);
+                Assert.AreEqual<string>("1¡ã40¡ä00¡å", dms);
+               
+                dms = ZXY.SurMath.RadtoString(-0.02908882086657220);
+                Assert.AreEqual<string>("-1¡ã40¡ä00¡å", dms);
+            }
+
+            {
+                string dms = ZXY.SurMath.RadtoString(0.02908930568025330);
+                Assert.AreEqual<string>("1¡ã40¡ä00.1¡å", dms);
+
+                dms = ZXY.SurMath.RadtoString(-0.02908930568025330);
+                Assert.AreEqual<string>("-1¡ã40¡ä00.1¡å", dms);
+            }
+
+            {                
+                string dms = ZXY.SurMath.RadtoString(0.69580806988502100); //39 52 0.71672
+                Assert.AreEqual<string>("39¡ã52¡ä00.71672¡å", dms);
+
+                dms = ZXY.SurMath.RadtoString(-0.69580806988502100); //-39 52 0.71672
+                Assert.AreEqual<string>("-39¡ã52¡ä00.71672¡å", dms);
+            }
         }
 
         [TestMethod]
