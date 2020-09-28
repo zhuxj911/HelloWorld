c6e460d04aa7c84fe6fad933c92d8711bf23a2c5
zhuxj <xazhuxj@qq.com>
2020-09-28 08:33:00 +0800

    master分支上的最后修改，以后将采用分支开发的方式，合并到master分支

---
+++ UnitTestSurMath/UnitTestSurMath.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSurMath
+{
+    [TestClass]
+    public class UnitTestSurMath
+    {
+        [TestMethod]
+        public void TestDMStoRad()
+        {
+        }
+
+        [TestMethod]
+        public void TestRadtoDMS()
+        {
+        }
+
+        [TestMethod]
+        public void TestAzimuth()
+        {
+        }
+    }
+}
